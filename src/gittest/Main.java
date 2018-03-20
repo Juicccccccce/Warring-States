958b7c1bc13805f1dda0b46805108fb8f26d998b
u6124186 <u6124186@anu.edu.au>
2018-03-20 12:14:25 +1100

    test8(3) for Alice

--- src/gittest/Main.java
+++ src/gittest/Main.java
@@ -3,7 +3,7 @@ package gittest;
 public class Main {
     B b = new B();
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Goodbye");
     }
 }
 
