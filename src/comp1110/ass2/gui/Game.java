1231915e90b7db9d455af7f20ba0b3b569be4af9
xinlixu <u6105656@anu.edu.au>
2018-05-06 14:29:03 +1000

    work for cleaning zhangyi and add zhangyi to destination

--- src/comp1110/ass2/gui/Game.java
+++ src/comp1110/ass2/gui/Game.java
@@ -21,6 +21,7 @@ import java.util.Scanner;
 public class Game extends Application {
     private static final int BOARD_WIDTH = 933;
     private static final int BOARD_HEIGHT = 700;
+    public static String placement = "g0Aa0Bf1Ca1Dc5Ee1Fa4Ge3He2Ia2Jc2Kd0Lf0Mb4Nd4Oa6Pc3Qe0Ra5Sc1Td1Uc4Vb5Wb0Xa7Yf2Zb10a31z92b33b64d35g16b27d28c09";
 
     // FIXME Task 9: Implement a basic playable Warring States game in JavaFX
 
@@ -31,7 +32,6 @@ public class Game extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Warring States");
-        String placement = "g0Aa0Bf1Ca1Dc5Ee1Fa4Ge3He2Ia2Jc2Kd0Lf0Mb4Nd4Oa6Pc3Qe0Ra5Sc1Td1Uc4Vb5Wb0Xa7Yf2Zb10a31z92b33b64d35g16b27d28c09";
         Group root = new Group();
         GridPane grid = setup(placement);
         root.getChildren().add(grid);
@@ -40,12 +40,37 @@ public class Game extends Application {
                 @Override
                 public void handle(MouseEvent event) {
                     int[] array = mouseEntered(event);
-                    if (array[0] >=0 && array[0] <= 5 && array[1] >= 0 && array[1] <= 5) {
-                    grid.getChildren().remove(array[0],array[1]);     //PROBLEM
-                }}
+                    char zhang = WarringStatesGame.findZhangPosition(placement);
+                    int[] Zhang = Viewer.determineCoordinate(zhang);
+//                    if (array[0] >=0 && array[0] <= 5 && array[1] >= 0 && array[1] <= 5) {
+//                        grid.getChildren().remove(getNodeByRowColumnIndex(Zhang[0],Zhang[1],grid));
+                    Image empty = new Image("comp1110/ass2/gui/assets/Character/empty.png"); //Remove previous ZhangYi's card
+                    ImageView imageView1 = new ImageView(empty);
+                    imageView1.setFitWidth(100);
+                    imageView1.setFitHeight(100);
+                    grid.getChildren().remove(getNodeByRowColumnIndex(array[1],array[0],grid));
+                    grid.add(imageView1,Zhang[1],Zhang[0]);    //Replace the selcted position card with ZhangYi's card
+//                        String supportors = WarringStatesGame.allPosition(returnLocationChar(array[0],array[1]),placement);  //Get all cards which are needed to collect
+//                        for (int i =0; i < supportors.length(); i+=2) {       //replace all cards need to be emptied
+//                            int[] a = Viewer.determineCoordinate(returnLocation(placement,supportors.substring(0,2)));
+////                            ImageView imageView2 = new ImageView(empty);
+////                            imageView2.setFitHeight(100);
+////                            imageView2.setFitWidth(100);
+//                            grid.getChildren().remove(getNodeByRowColumnIndex(a[0],a[1],grid));
+//                            grid.add(imageView1,a[1],a[0]);
+//                            placement = WarringStatesGame.deleteEmptyLocation(placement,returnLocationChar(array[0],array[1]));  //update the set up information
+//                            placement += "z9" + returnLocationChar(array[0],array[1]);
+//                        }
+                        Image ZhangYi = new Image("comp1110/ass2/gui/assets/Character/z9.png");
+                        ImageView imageview = new ImageView(ZhangYi);
+                        imageview.setFitHeight(100);
+                        imageview.setFitWidth(100);
+                        grid.add(imageview,array[0],array[1]);
+                        placement = WarringStatesGame.deleteEmptyLocation(placement,returnLocationChar(array[0],array[1]));  //update the set up information
+                        placement += "z9" + returnLocationChar(array[0],array[1]);
+                }
             });
-                                   });
-
+        });
         Scene scene = new Scene(root, 935, 732);
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -54,6 +79,7 @@ public class Game extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
     public Node getNodeByRowColumnIndex (final int row, final int column, GridPane gridPane) {
         Node result = null;
         ObservableList<Node> childrens = gridPane.getChildren();
@@ -66,6 +92,109 @@ public class Game extends Application {
         }
 
         return result;
+    }
+
+    //return the locationchar for where the support is in the placement
+    public static char returnLocation(String placement, String supporter) {
+        char re = 'x';
+        for (int i = 0; i < placement.length(); i+=3) {
+            if (placement.charAt(i) == supporter.charAt(0) && placement.charAt(i+1) == supporter.charAt(1)) {
+                re = placement.charAt(i+2);
+            }
+        }
+        return re;
+    }
+
+
+    //return the locationchar for a certian column and row
+    public static char returnLocationChar(int col, int row) {
+        char a = '=';
+        if (col == 0) {
+            if (row == 0) {
+                a = '4';
+            } else if (row == 1) {
+                a = '5';
+            } else if (row == 2) {
+                a = '6';
+            } else if (row == 3) {
+                a = '7';
+            } else if (row == 4) {
+                a = '8';
+            } else if (row == 5) {
+                a = '9';
+            }
+        } else if (col == 1) {
+            if (row == 0) {
+                a = 'Y';
+            } else if (row == 1) {
+                a = 'Z';
+            } else if (row == 2) {
+                a = '0';
+            } else if (row == 3) {
+                a = '1';
+            } else if (row == 4) {
+                a = '2';
+            } else if (row == 5) {
+                a = '3';
+            }
+        } else if (col == 2) {
+            if (row == 0) {
+                a = 'S';
+            } else if (row == 1) {
+                a = 'T';
+            } else if (row == 2) {
+                a = 'U';
+            } else if (row == 3) {
+                a = 'V';
+            } else if (row == 4) {
+                a = 'W';
+            } else if (row == 5) {
+                a = 'X';
+            }
+        } else if (col == 3) {
+            if (row == 0) {
+                a = 'M';
+            } else if (row == 1) {
+                a = 'N';
+            } else if (row == 2) {
+                a = 'O';
+            } else if (row == 3) {
+                a = 'P';
+            } else if (row == 4) {
+                a = 'Q';
+            } else if (row == 5) {
+                a = 'R';
+            }
+        } else if (col == 4) {
+            if (row == 0) {
+                a = 'G';
+            } else if (row == 1) {
+                a = 'H';
+            } else if (row == 2) {
+                a = 'I';
+            } else if (row == 3) {
+                a = 'J';
+            } else if (row == 4) {
+                a = 'K';
+            } else if (row == 5) {
+                a = 'L';
+            }
+        } else if (col == 5) {
+            if (row == 0) {
+                a = 'A';
+            } else if (row == 1) {
+                a = 'B';
+            } else if (row == 2) {
+                a = 'C';
+            } else if (row == 3) {
+                a = 'D';
+            } else if (row == 4) {
+                a = 'E';
+            } else if (row == 5) {
+                a = 'F';
+            }
+        }
+      return a;
     }
 
     //set the initial board
